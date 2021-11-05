b316d14f9a8cdca42f168e6d7d82c53a5894900f
Catalina Luta <catalina.luta@yahoo.com>
2021-11-05 21:54:28 +0200

    added property-add in Controller

--- src/main/java/com/sda/project/controller/PropertyController.java
+++ src/main/java/com/sda/project/controller/PropertyController.java
@@ -2,30 +2,40 @@ package com.sda.project.controller;
 
 import com.sda.project.model.Property;
 import com.sda.project.service.PropertyService;
+import com.sda.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Scanner;
 
 @Controller
 public class PropertyController {
 
     private final PropertyService propertyService;
+    private final UserService userService;
 
     @Autowired
-    public PropertyController(PropertyService propertyService) {
+    public PropertyController(PropertyService propertyService, UserService userService) {
         this.propertyService = propertyService;
+        this.userService = userService;
     }
 
     @GetMapping("/properties")
     public String getPropertiesPage(Model model) {
-        // get list of properties
+        // get list of properties //1
         List<Property> properties = propertyService.findAll();
 
-        // add to model
+        // add to model//2
         model.addAttribute("properties", properties);
         return "property/properties";
+    }
+    @GetMapping("/properties/add")
+    public String showAddPage(Model model) {
+        model.addAttribute("property", new Property());
+        model.addAttribute("users", userService.findAll());
+        return "property/property-add";
     }
 }
